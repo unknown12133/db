7d33add5301cb6a47732ccf9ca9149c926f71218
unknown12133 <mr.unknown.boy.022@gmail.com>
2025-04-25 11:52:14 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+    header('content-type:application/json');
+    
+
+?>
+
+<html>
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+    <span id="dd">hi sir</span>
+</body>
+</html>
