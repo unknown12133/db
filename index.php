d34569f3854004395f5d982e1ef74480a5777171
unknown12133 <mr.unknown.boy.022@gmail.com>
2025-05-02 21:28:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-[root@localhost bin]#cd /var/www/html    
-
-[root@localhost bin]#cd /var/www/html
-
-[root@localhost bin]#vi test.php
-
 <!DOCTYPE html>
 
 <html>
@@ -14,9 +8,9 @@
 
 echo "My first PHP script!";
 
-pg_connect("host=dpg-d046ndbuibrs73aq4v60-a dbname=projectdb_75bu user=projectdb password=kcTYffQ7HPvaXSsiFAOPOir52XQ0UOP8");
+$con=pg_connect("host=dpg-d046ndbuibrs73aq4v60-a dbname=projectdb_75bu user=projectdb password=kcTYffQ7HPvaXSsiFAOPOir52XQ0UOP8");
 
-pg_query("create table testing(id integer)");
+$sql=pg_query("create table testing(id integer)");
 
 echo " script! Executed";
 
