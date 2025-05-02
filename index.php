de43b8c017806a32674e243b73c4e94a83f6a4ea
unknown12133 <mr.unknown.boy.022@gmail.com>
2025-05-02 22:16:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,18 @@ try {
     // use $pdo to query the database
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
-  echo 'aa';
 }
-  echo 'ee';
+$sql=("CREATE TABLE aaa (
+    SNO INT(5) NOT NULL AUTO_INCREMENT,
+    UID VARCHAR(14) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    MOBILE_NO CHAR(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    NAME CHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    FATHER_NAME CHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    AMOUNT INT(10) NOT NULL,
+    PASSWORD VARCHAR(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    DATE DATE NOT NULL DEFAULT CURRENT_DATE,
+    TIME TIME NOT NULL DEFAULT CURRENT_TIME,
+    PRIMARY KEY (SNO)")
+);
+
 ?>
